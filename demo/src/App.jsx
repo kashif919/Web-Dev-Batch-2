d91fd55848f948cd9fb2d924b2e4ea7701d18190
netbots <netbotstech@gmail.com>
2024-12-18 15:12:32 +0500

    react-vite start

---
+++ demo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Text from "./components/text"
+
+function App() {
+
+  return (
+    <>
+      <Text name="Akbar" />
+    </>
+  )
+}
+
+export default App
---
+++ demo/src/components/Text.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Text = ({name}) => {
+    // console.log('props', props)
+    return (
+        <h1>My name is {name}</h1>
+    )
+}
+
+export default Text
