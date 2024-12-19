22a014bfbe9e20ad7ea0eb76b2edff64f4f236d2
netbots <netbotstech@gmail.com>
2024-12-19 15:33:38 +0500

    states

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import Text from "./components/text"
+import States from "./components/states"
 
 function App() {
 
   return (
     <>
+      <h1>Props</h1>
       <Text name="Akbar" />
+      <h1>States</h1>
+      <States/>
     </>
   )
 }
